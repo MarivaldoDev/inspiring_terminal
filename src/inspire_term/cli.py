4c6448d74694188c3884e8682fb380b7f30de944
Marivaldo Pedro <pedromarivaldo10@gmail.com>
2026-08-08 15:57:53 -0300

    :recycle: refactor: separando responsabilidades e adicionando sub-comandos para o CLI

--- src/inspire_term/cli.py
+++ src/inspire_term/cli.py
@@ -1,56 +1,27 @@
-from dataclasses import replace
-from datetime import date
-from random import choice
+from typing import Annotated
 
-from inspire_term.cache import QuoteCache
-from inspire_term.exceptions import QuoteFetchError, TranslationError
-from inspire_term.models import QuoteCacheData
-from inspire_term.quote import QuoteService
-from inspire_term.translator import TranslatorService
-from inspire_term.ui import ConsoleRenderer
+import typer
+
+from inspire_term.flow import run, version_callback
+
+app = typer.Typer()
 
 
-def main() -> None:
-    renderer = ConsoleRenderer()
-    quote_service = QuoteService()
-    translator = TranslatorService()
-    cache = QuoteCache()
-
-    cache_data = cache.load()
-
-    if cache_data is None:
-        try:
-            quotes = quote_service.get_quotes()
-        except QuoteFetchError as e:
-            renderer.error(str(e))
-            return
-
-        cache_data = QuoteCacheData(
-            date=date.today(),
-            quotes=quotes,
-        )
-
-    try:
-        quote = choice(cache_data.quotes)
-        selected_index = cache_data.quotes.index(quote)
-    except IndexError:
-        renderer.error(
-            "Todas as frases do dia foram usadas. Aguardamos você no dia de amanhã!"
-        )
-        return
-
-    try:
-        translated = translator.translate(quote.text)
-        quote = replace(quote, translated=translated)
-    except TranslationError:
-        renderer.error("Tradução falhou! Exibindo frase em Inglês.")
-        quote = replace(quote, translated=quote.text)
-
-    renderer.show(quote.translated or quote.text, quote.author)
-
-    cache_data.quotes.pop(selected_index)
-    cache.save(cache_data)
+@app.command()
+def main(
+    version: Annotated[
+        bool,
+        typer.Option(
+            "--version",
+            callback=version_callback,
+            is_eager=True,
+            help="Show the application version.",
+        ),
+    ] = False,
+    no_translate: bool = False,
+) -> None:
+    run(no_translate=no_translate)
 
 
 if __name__ == "__main__":
-    main()
+    app()
